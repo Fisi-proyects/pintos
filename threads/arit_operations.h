60d4fec0f922b5744fa42a50223d94f20d85f95c
Jeremy <jeremyjr02@outlook.com>
2024-09-26 08:15:20 -0500

    shorter fixed-point factor variable

--- threads/arit_operations.h
+++ threads/arit_operations.h
@@ -1,5 +1,5 @@
 #include <stdint.h>
-#define FIXED_POINT_FACTOR (1 << 14) // factor de conversion
+#define F (1 << 14) // factor de conversion
 
 int int_to_fixed_point(int n);          // convertir entero a punto fijo
 int fixed_point_to_int(int x);          // convertir punto fijo a entero
@@ -15,23 +15,23 @@ int divide_fixed_point_and_int(int x, int n);  // division de un numero en punto
 
 int int_to_fixed_point(int n)
 {
-   return n * FIXED_POINT_FACTOR;
+   return n * F;
 }
 
 int fixed_point_to_int(int x)
 {
-   return x / FIXED_POINT_FACTOR;
+   return x / F;
 }
 
 int fixed_point_to_int_round(int x)
 {
    if (x >= 0)
    {
-      return (x + FIXED_POINT_FACTOR / 2) / FIXED_POINT_FACTOR;
+      return (x + F / 2) / F;
    }
    else
    {
-      return (x - FIXED_POINT_FACTOR / 2) / FIXED_POINT_FACTOR;
+      return (x - F / 2) / F;
    }
 }
 
@@ -47,17 +47,17 @@ int subtract_fixed_points(int x, int y)
 
 int add_fixed_point_and_int(int x, int n)
 {
-   return x + (n * FIXED_POINT_FACTOR);
+   return x + (n * F);
 }
 
 int subtract_fixed_point_and_int(int x, int n)
 {
-   return x - (n * FIXED_POINT_FACTOR);
+   return x - (n * F);
 }
 
 int multiply_fixed_points(int x, int y)
 {
-   return ((int64_t)x) * y / FIXED_POINT_FACTOR;
+   return ((int64_t)x) * y / F;
 }
 
 int multiply_fixed_point_and_int(int x, int n)
@@ -67,7 +67,7 @@ int multiply_fixed_point_and_int(int x, int n)
 
 int divide_fixed_points(int x, int y)
 {
-   return ((int64_t)x) * FIXED_POINT_FACTOR / y;
+   return ((int64_t)x) * F / y;
 }
 
 int divide_fixed_point_and_int(int x, int n)
